b67218315a951753cc99e9f5bc3dedbedacd2fc5
Büşra Erçelik <bsr.ercelik@gmail.com>
2024-06-05 23:52:16 +0400

    add other microservices

--- start.cmd
+++ start.cmd
@@ -1,5 +1,27 @@
 @echo off
+
 cd event-management-svc
 call mvnw clean package
 cd ..
+
+cd event-svc
+call mvnw clean package
+cd ..
+
+cd notification-svc
+call mvnw clean package
+cd ..
+
+cd payment-svc
+call mvnw clean package
+cd ..
+
+cd ticket-svc
+call mvnw clean package
+cd ..
+
+cd user-svc
+call mvnw clean package
+cd ..
+
 docker-compose up -d
--- start.sh
+++ start.sh
@@ -1,4 +1,25 @@
 cd event-management-svc
 mvnw clean package
 cd ..
+
+cd event-svc
+mvnw clean package
+cd ..
+
+cd notification-svc
+mvnw clean package
+cd ..
+
+cd payment-svc
+mvnw clean package
+cd ..
+
+cd ticket-svc
+mvnw clean package
+cd ..
+
+cd user-svc
+mvnw clean package
+cd ..
+
 docker-compose up -d
