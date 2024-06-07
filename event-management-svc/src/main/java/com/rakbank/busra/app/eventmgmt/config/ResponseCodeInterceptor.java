bdd22d73e45970ce08b6f1c97d6400a65e00184d
Büşra Erçelik <bsr.ercelik@gmail.com>
2024-06-07 20:39:08 +0400

    fix interceptor

--- event-management-svc/src/main/java/com/rakbank/busra/app/eventmgmt/config/ResponseCodeInterceptor.java
+++ event-management-svc/src/main/java/com/rakbank/busra/app/eventmgmt/config/ResponseCodeInterceptor.java
@@ -5,13 +5,17 @@ import com.rakbank.busra.app.eventmgmt.common.dto.BaseAPIResponse;
 import com.rakbank.busra.app.eventmgmt.common.errors.BaseErrorCode;
 import com.rakbank.busra.app.eventmgmt.common.exceptions.ApplicationException;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Component
 @AllArgsConstructor
@@ -20,12 +24,16 @@ public class ResponseCodeInterceptor implements ClientHttpRequestInterceptor {
 
     @Override
     public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
-        ClientHttpResponse response = execution.execute(request, body);
-        var apiResponse = objectMapper.readValue(response.getBody(), BaseAPIResponse.class);
+        var response = execution.execute(request, body);
+        var responseInputStream = response.getBody();
+
+        byte[] responseByteArray = responseInputStream.readAllBytes();
+        var apiResponse = objectMapper.readValue(responseByteArray, BaseAPIResponse.class);
+
         if(!("200".equals(apiResponse.getCode()))){
             throw new ApplicationException(apiResponse.getDetail(), getErrorCode(apiResponse, response));
         }
-        return response;
+        return new ClientHttpResponseWrapper(response, responseByteArray);
     }
 
     private static BaseErrorCode getErrorCode(BaseAPIResponse<?> apiResponse, ClientHttpResponse response) {
@@ -45,4 +53,41 @@ public class ResponseCodeInterceptor implements ClientHttpRequestInterceptor {
             }
         };
     }
+
+    public static class ClientHttpResponseWrapper implements ClientHttpResponse {
+
+        private final ClientHttpResponse originalResponse;
+        private final byte[] responseBody;
+
+        public ClientHttpResponseWrapper(ClientHttpResponse originalResponse, byte[] responseBody) {
+            this.originalResponse = originalResponse;
+            this.responseBody = responseBody;
+        }
+
+        @Override
+        public HttpStatusCode getStatusCode() throws IOException {
+            return originalResponse.getStatusCode();
+        }
+
+        @Override
+        public String getStatusText() throws IOException {
+            return originalResponse.getStatusText();
+        }
+
+        @Override
+        public void close() {
+            originalResponse.close();
+        }
+
+        @Override
+        public InputStream getBody() throws IOException {
+            return new ByteArrayInputStream(responseBody);
+        }
+
+        @Override
+        public HttpHeaders getHeaders() {
+            return originalResponse.getHeaders();
+        }
+    }
+
 }
