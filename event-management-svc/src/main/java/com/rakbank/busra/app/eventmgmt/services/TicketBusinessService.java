9b01909712e33047b97bc8b302a3f7e4e3eb3c61
Büşra Erçelik <bsr.ercelik@gmail.com>
2024-06-08 00:09:26 +0400

    notify on successfull booking

--- event-management-svc/src/main/java/com/rakbank/busra/app/eventmgmt/services/TicketBusinessService.java
+++ event-management-svc/src/main/java/com/rakbank/busra/app/eventmgmt/services/TicketBusinessService.java
@@ -1,13 +1,17 @@
 package com.rakbank.busra.app.eventmgmt.services;
 
 import com.rakbank.busra.app.eventmgmt.clients.eventservice.EventClient;
+import com.rakbank.busra.app.eventmgmt.clients.eventservice.dtos.commons.EventDTO;
 import com.rakbank.busra.app.eventmgmt.clients.notificationservice.NotificationClient;
+import com.rakbank.busra.app.eventmgmt.clients.notificationservice.dtos.commons.NotificationType;
+import com.rakbank.busra.app.eventmgmt.clients.notificationservice.dtos.commons.requests.NotificationRequestDTO;
 import com.rakbank.busra.app.eventmgmt.clients.paymentservice.PaymentClient;
 import com.rakbank.busra.app.eventmgmt.clients.paymentservice.dtos.commons.PaymentDTO;
 import com.rakbank.busra.app.eventmgmt.clients.ticketservice.TicketClient;
 import com.rakbank.busra.app.eventmgmt.clients.ticketservice.dtos.commons.TicketTypeDTO;
 import com.rakbank.busra.app.eventmgmt.clients.ticketservice.dtos.responses.TicketSaleResponseDTO;
 import com.rakbank.busra.app.eventmgmt.clients.userservice.UserClient;
+import com.rakbank.busra.app.eventmgmt.clients.userservice.dtos.commons.UserDTO;
 import com.rakbank.busra.app.eventmgmt.dtos.requests.BookTicketBusinessRequest;
 import com.rakbank.busra.app.eventmgmt.dtos.requests.CancelTicketBusinessRequest;
 import com.rakbank.busra.app.eventmgmt.dtos.responses.BookTicketBusinessResponse;
@@ -31,7 +35,7 @@ public class TicketBusinessService {
 
     public BookTicketBusinessResponse bookTicket(BookTicketBusinessRequest request) {
         //fetch the user - validate if it exists
-        var user = userClient.getById(request.getUserId());
+        var user = userClient.getById(request.getUserId()).getResult();
         // fetch the event - validate if it exists
         var event = eventClient.getById(request.getEventId()).getResult();
         log.info("Booking ticket for event : {} for user : {}", event.getName(), request.getUserId());
@@ -47,12 +51,32 @@ public class TicketBusinessService {
         ticketClient.linkPaymentWithTicketSale(ticketSaleResponse.getReferenceId(), paymentResponse.getId());
 
         //notify user of booking
-        //notificationClient.notifyUser(get);
+        var notification = getBookingSuccessfulNotification(user, event, ticketSaleResponse);
+        notificationClient.notifyUser(notification);
         return new BookTicketBusinessResponse(
                 request.getUserId(), request.getEventId(), paymentResponse.getId(), ticketSaleResponse.getReferenceId());
     }
 
-
+    private static NotificationRequestDTO getBookingSuccessfulNotification(UserDTO user, EventDTO event, TicketSaleResponseDTO ticketSaleResponse) {
+        var notification = new NotificationRequestDTO();
+        notification.setNotificationType(NotificationType.EMAIL);
+        notification.setRecipient(user.getEmail());
+        notification.setSubject(String.format("Event %s booked successfully for date %s",
+                event.getName(), event.getDateFrom()));
+        notification.setMessage(String.format("""
+                You have successfully booked a ticket - %s
+                
+                ● Event Name : %s
+                ● Event Date : %s
+                ● Event Location :%s
+                ● User Name : %s
+                ● Ticket Type : %s
+                ● Number of Ticket(s) : 1
+                ● Payment Amount : %s
+                """, ticketSaleResponse.getReferenceId(), event.getName(), event.getDateFrom(),
+                "", user.getEmail(), ticketSaleResponse.getTicketTypeName(), ticketSaleResponse.getAmount()));
+        return notification;
+    }
 
 
     public CancelTicketBusinessResponse cancelTicket(CancelTicketBusinessRequest request) {
