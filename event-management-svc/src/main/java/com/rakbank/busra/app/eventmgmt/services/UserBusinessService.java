43efa6d4c07885068084037d6f4ad695dbd3bbcb
Busra Ercelik <bsr.ercelik@gmail.com>
2024-06-08 06:43:02 +0400

    add @RequestBody annotation to user create

--- event-management-svc/src/main/java/com/rakbank/busra/app/eventmgmt/services/UserBusinessService.java
+++ event-management-svc/src/main/java/com/rakbank/busra/app/eventmgmt/services/UserBusinessService.java
@@ -4,16 +4,20 @@ import com.rakbank.busra.app.eventmgmt.clients.userservice.UserClient;
 import com.rakbank.busra.app.eventmgmt.common.dto.BaseAPIResponse;
 import com.rakbank.busra.app.eventmgmt.clients.userservice.dtos.commons.UserDTO;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class UserBusinessService {
 
     private final UserClient userClient;
 
-    public BaseAPIResponse<UserDTO> createUser(UserDTO dto) {
-        return userClient.createUser(dto);
+    public UserDTO createUser(UserDTO dto) {
+        var user = userClient.createUser(dto).getResult();
+        log.info("User created {}", user);
+        return user;
     }
 
     public BaseAPIResponse<UserDTO> getUser(Long id) {
